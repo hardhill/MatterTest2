1f9f0ce02f7e9d4580d0bac0f24addc149c36c59
hardhill <hardhill@mail.ru>
2020-08-12 22:50:43 +0800

    index

--- box.js
+++ box.js
@@ -10,6 +10,11 @@ function Box(x,y,w,h){
         var angle = this.body.angle;
         push()
         translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER)
+        strokeWeight(1)
+        stroke(25)
+        fill(177)
         rect(0,0, this.w, this.h)
         pop()
     }
